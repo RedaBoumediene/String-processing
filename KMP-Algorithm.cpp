7605727e52937d473b623a69e185c8737fff9a17
Reda BOUMEDIENE <reda.boumediene@gmail.com>
2019-09-09 19:45:26 +0100

    KMP Algorithm

---
+++ KMP-Algorithm.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s, t;
+int f[100];
+void search() {
+	int len = 0;
+	for (int i = 0; i < s.size(); ++i) {
+		while (len>0 && s[i] != t[len])
+			len = f[len - 1];
+		if (s[i] == t[len])
+			++len;
+		if (len == t.size()) {
+			cout << i - len + 1 << endl;
+			len = f[len - 1];
+		}
+	}
+}
+void build() {
+	int len = 0;
+	f[0] = 0;
+	for (int i = 1; i < t.size(); ++i) {
+		while (len>0 && t[i] != t[len])
+			len = f[len - 1];
+		if (t[i] == t[len])
+			++len;
+		f[i] = len;
+	}
+}
+int main() {
+	//   01234567890123456789012345
+	s = "carcarcarcarcardcarcardcar";
+	t = "carcard";
+	build();
+	search();
+	return 0;
+}
